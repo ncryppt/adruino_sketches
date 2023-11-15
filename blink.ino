be656516633e1355acb12dd9cdfcbacfbc614fb6
Nick <66481391+ncryppt@users.noreply.github.com>
2023-11-15 14:44:43 -0500

    Create blink.ino

---
+++ blink.ino
@@ -0,0 +1,12 @@
+#define LED 13
+void setup() {
+ 
+  pinMode(LED, OUTPUT); // sets digital pin output
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+}
